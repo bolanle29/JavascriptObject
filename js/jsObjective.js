3f46bd712f94590848dc946511d11d2435e8941c
Bolanle Ayodele <royaleunice29@gmail.com>
2021-09-05 21:33:46 +0100

    initial commit

---
+++ js/jsObjective.js
@@ -0,0 +1,81 @@
+let programming ={
+    languages:["JavaScript", "Python","Ruby"],
+    isChallenging:true,
+    isRewarding:true,
+    difficulty:8,
+    jokes:"http://stackoverflow.com/question/234075/what-is-your-best-programmer-joke"
+};
+
+// Question 1
+// Write Command to add  the  language "Go" to the end of the array
+
+// programming. languages. push("Go")
+// console.log(programming)
+
+
+
+
+// Quetion 2
+// Change the 'difficulty' to the value of 7
+
+// programming["difficulty"]=7;
+// console.log(programming)
+
+
+
+
+// Question 3
+// Using the "delete" keyword, write the command to remove the jokes the from the programming object
+
+// delete programming.jokes;
+// console.log(programming)
+
+
+
+
+// Question 4
+// Write the command to add a new key called "isFun" and value "true" to the programming object
+
+// programming.isFun =true
+// console.log(programming)
+
+
+
+// Question 5
+// Using a loop, iterate through the languages array and console.log all of the languages
+
+// function progLang(langs){
+//     let tempArr="";
+//     let obj={};
+//     let i=0;
+//     for (let prog in langs){
+//         console.log(prog)
+//         langs[prog]=(langs[prog])
+//         console.log(langs[prog])
+//     }
+
+// }
+
+
+
+
+// Question 6
+// Using a loop, console.log all the key in the programming object
+
+// for (let key in programming){
+//     console.log(key)
+// }
+
+
+
+
+
+// Question 7
+// Using a loop, console.log all the values in the programming object
+
+//  for (let key in programming){
+//      console.log(programming[key])
+    
+//  }
+
+
